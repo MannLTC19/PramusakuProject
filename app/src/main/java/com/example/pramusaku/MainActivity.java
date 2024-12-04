98ba84511ceb3f40b361c05212e4234a4396d4df
joeeey <joeeey.jo.ez@gmail.com>
2024-12-04 09:32:06 +0700

    Base Version 0.0.1 added Fragments

--- app/src/main/java/com/example/pramusaku/MainActivity.java
+++ app/src/main/java/com/example/pramusaku/MainActivity.java
@@ -7,7 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.pramusaku.Tools.ToolsFragment;
 import com.example.pramusaku.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,15 +21,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationBar.setBackground(null);
 
+        binding.bottomNavigationBar.setOnItemSelectedListener(item -> {
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+
+                case R.id.tools:
+                    replaceFragment(new ToolsFragment());
+                    break;
+
+                case R.id.aboutUs:
+                    replaceFragment(new AboutUsFragment());
+                    break;
+
+                case R.id.c:
+                    replaceFragment(new CertificateFragment());
+                    break;
+
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
         });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
