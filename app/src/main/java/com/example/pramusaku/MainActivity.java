db9457ae41929b233cb5f9e5cd7e175ea368a65a
joeeey <joeeey.jo.ez@gmail.com>
2024-12-04 09:32:06 +0700

    Base Version 0.0.2

--- app/src/main/java/com/example/pramusaku/MainActivity.java
+++ app/src/main/java/com/example/pramusaku/MainActivity.java
@@ -2,11 +2,10 @@ package com.example.pramusaku;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.pramusaku.databinding.ActivityMainBinding;
 
@@ -17,15 +16,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationBar.setBackground(null);
 
+        binding.bottomNavigationBar.setOnItemSelectedListener(item -> {
+            int id = item.getItemId();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+            if (id == R.id.homes) {
+                replaceFragment(new HomeFragment());
+            } else if (id == R.id.tools) {
+                replaceFragment(new ToolsFragment());
+            } else if (id == R.id.aboutUs) {
+                replaceFragment(new AboutUsFragment());
+            } else if (id == R.id.certificates) {
+                replaceFragment(new CertificatesFragment());
+            } else if (id == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            } else {
+                return false; // Unhandled case, return false
+            }
+
+            return true; // Menu item handled
         });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
